a330df354effe33739f07ab8bab6233af9e0e51a
globaluser <globaluser@gmail.com>
2025-08-12 15:34:41 +0000

    WIP: local changes to App.java

--- src/main/java/com/pro/App.java
+++ src/main/java/com/pro/App.java
@@ -5,6 +5,6 @@ package com.pro;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello India!");
     }
 }
